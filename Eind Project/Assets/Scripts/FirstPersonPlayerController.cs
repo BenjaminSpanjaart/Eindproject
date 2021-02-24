ad4625b7fcf202eec714b937e3e7ec32d473f2ab
Benjamin Spanjaart <benjaminspanjaart@gmail.com>
2021-02-24 16:23:05 +0100

    + Added FirstPersonController
    + Added Main Menu
    + Added Main Menu Theme
    + Added PlayerinputHandler
    + Added MainMenuManager
    + Added Fonts

---
+++ Eind Project/Assets/Scripts/FirstPersonPlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonPlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerBody;
+    [SerializeField] private float playerspeed;
+    private Rigidbody PlayerRB;
+    Vector3 Mousepos;
+
+    private float JumpCooldown = 1f;
+    void Start()
+    {
+        PlayerRB = PlayerBody.GetComponent<Rigidbody>();
+    }
+    void Update()
+    { 
+        if (JumpCooldown <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                PlayerRB.AddForce(new Vector3(0, 5f, 0), ForceMode.Impulse);
+                JumpCooldown = 3f;
+            }
+        }
+        else
+        {
+            JumpCooldown -= Time.deltaTime;
+        }
+
+        Mousepos = Input.mousePosition;
+       
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            PlayerBody.transform.position = new Vector3(PlayerBody.transform.position.x, PlayerBody.transform.position.y, PlayerBody.transform.position.z + playerspeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            PlayerBody.transform.position = new Vector3(PlayerBody.transform.position.x, PlayerBody.transform.position.y, PlayerBody.transform.position.z - playerspeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlayerBody.transform.position = new Vector3(PlayerBody.transform.position.x - playerspeed, PlayerBody.transform.position.y, PlayerBody.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerBody.transform.position = new Vector3(PlayerBody.transform.position.x + playerspeed, PlayerBody.transform.position.y, PlayerBody.transform.position.z);
+        }
+    }
+}
---
+++ Eind Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInputHandler))]
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController cController;
+
+    private PlayerInputHandler input;
+
+    private Camera camera;
+    
+    [SerializeField]private float fMovementSpeed;
+
+    private float rUp;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        
+        cController = GetComponent<CharacterController>();
+
+        camera = Camera.main;
+        
+        input = GetComponent<PlayerInputHandler>();
+
+        input.movementInput.AddListener(Movement);
+        input.rotationInput.AddListener(Rotation);
+    }
+
+    private void Movement(float xInput, float yInput)
+    {
+        var mForward = yInput * cController.transform.forward;
+        var mRight = xInput * cController.transform.right;
+
+        Vector3 vMovement = Vector3.Normalize(mForward + mRight);
+
+        cController.SimpleMove(vMovement * fMovementSpeed);
+    }
+
+    private void Rotation(float xInput, float yInput)
+    {
+        rUp = Mathf.Clamp(rUp - yInput, -80, 80);
+        var rRight = xInput;
+        
+        cController.transform.rotation = 
+            Quaternion.Euler(0.0f,rRight,0.0f) * 
+            cController.transform.rotation;
+        
+        camera.transform.localRotation = Quaternion.Euler(rUp,0.0f,0.0f);
+    }
+}
