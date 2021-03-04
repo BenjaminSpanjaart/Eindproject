7a8fdd34665e7e2ed7b29f30d37f3b3b1195a48f
Vincent Poorthuis <vincentpoorthuis@live.nl>
2021-03-04 13:41:14 +0100

    scene manager

---
+++ Eind Project/Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscapeMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject Escape;
+    [SerializeField] private GameObject Options;
+    GameObject opt;
+    GameObject esc;
+    private SceneManagment Scenes;
+    private void Start()
+    {
+        GameObject scenemanager = GameObject.Find("SceneManager");
+        Scenes = scenemanager.GetComponent<SceneManagment>();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && !GameObject.Find("EscMenu"))
+        {
+            esc = GameObject.Instantiate(Escape);
+        }else
+        {
+            GameObject.Destroy(esc);
+            if (GameObject.Find("OptionsMenu"))
+            {
+                GameObject.Destroy(opt);
+            }
+        }
+    }
+    public void OptionsMenu()
+    {
+        if (!GameObject.Find("OptionsMenu"))
+        {
+            opt = GameObject.Instantiate(Options);
+        }
+        else
+        {
+            GameObject.Destroy(opt);
+        }
+    }
+    public void QuitToMenu()
+    {
+        Scenes.StartScene();
+    }
+}
---
+++ Eind Project/Assets/Scripts/SceneManagment.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneManagment : MonoBehaviour
+{
+    [SerializeField] private GameObject ConfirmationBox;
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(ConfirmationBox);
+    }
+
+    public void GameScene()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void EndGameScene() 
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void StartScene()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void QuitGameConfirm()
+    {
+        ConfirmationBox.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void DontQuit()
+    {
+        ConfirmationBox.SetActive(false);
+    }
+
+}
